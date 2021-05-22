55396645e6b0a3999cfed67368643bdf85d269f3
BuildTools <unconfigured@null.spigotmc.org>
2021-05-22 02:17:45 -0300

    Ajustes finales de colores y visual

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 document.querySelector('.exitButtton').addEventListener("click", hiddeModal, "false");
+
 function hiddeModal() {
     document.querySelector('.modal-loggin').style.setProperty('display', 'none')
+    document.querySelector('.textBackground').style.setProperty('visibility', 'unset')
 }
 
 document.querySelector('#login').addEventListener("click", showModal, "false");
+
 function showModal() {
     document.querySelector('.modal-loggin').style.setProperty('display', 'block')
+    document.querySelector('.textBackground').style.setProperty('visibility', 'hidden')
 }
 
 document.querySelector('.button').addEventListener("click", () => { location.reload() }, "false");
