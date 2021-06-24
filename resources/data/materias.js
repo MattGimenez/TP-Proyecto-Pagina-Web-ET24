a3a5449cc19986e0f32a63e813b90734399191d5
MattGimenez <matgimenez53@gmail.com>
2021-06-23 22:34:06 -0300

    Archivo de materias

---
+++ resources/data/materias.js
@@ -0,0 +1,32 @@
+var subjects = [
+    {
+    "classHours": "4",
+    "name": "Matemática",
+    "prof": "Pierini"
+    },
+    {
+    "classHours": "3",
+    "name": "Inglés",
+    "prof": "Lopresti"
+    },
+    {
+    "classHours": "9",
+    "name": "Prácticas Profesionalizantes",
+    "prof": "Martínez"
+    },
+    {
+    "classHours": "6",
+    "name": "Desarrollo de Sistemas",
+    "prof": "Mombrú"
+    },
+    {
+    "classHours": "7",
+    "name": "Administración de Sistemas y Redes",
+    "prof": "Ganci"
+    },
+    {
+    "classHours": "2",
+    "name": "Ciencia y Tecnología",
+    "prof": "Lima Barreto"
+    }
+];
